2526d3599ac6c3d4d751aca814c13f008b66e021
Consalvo <gonzaloceciliano@gmail.com>
2017-05-13 18:05:01 -0500

    Better Layer Shorting

--- Assets/Scripts/LayerShorting.cs
+++ Assets/Scripts/LayerShorting.cs
@@ -2,19 +2,22 @@ using UnityEngine;
 using System.Collections;
 
 public class LayerShorting : MonoBehaviour {
-	public float layerDetail = 2;
+	public float layerDetail = 0.5f;
   private SpriteRenderer sprite;
-	private int posZ = 0;
+	public int posZ = 0;
 	private float boundHeight;
+	private float spriteHeight;
 
   void Start() {
   	sprite = GetComponent<SpriteRenderer>();
+		spriteHeight = sprite.bounds.size.z;
   }
 
   void Update() {
-		boundHeight = sprite.bounds.size.z * 0.4142f;
-  	if (sprite)
+  	if (sprite) {
+			boundHeight = sprite.bounds.size.z * 0.5f; //* Mathf.Cos(transform.rotation.eulerAngles.x * Mathf.Deg2Rad);
 			posZ = -(int)Mathf.Floor(transform.position.z - boundHeight * layerDetail);
   		sprite.sortingOrder = posZ;
     }
  	}
+}
